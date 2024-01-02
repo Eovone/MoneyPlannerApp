18f0ee4c0831fef23f3a804b85560acedbb058cf
Eovone <114399255+Eovone@users.noreply.github.com>
2024-01-02 10:46:32 +0100

    home fixed

--- src/Views/LoginView.tsx
+++ src/Views/LoginView.tsx
@@ -4,7 +4,6 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Image } from 'react-bootstrap';
 import LoginUserForm from '../Components/User/LoginUserForm';
-import { useLocation } from 'react-router-dom';
 
 const LoginView: FC = () => {  
   
@@ -21,40 +20,48 @@ const LoginView: FC = () => {
           </Col>
         </Row>
 
-        <Row>
-          <Col className='text-center col-lg-6 mt-3 col-12'>
-            <h3 className='mp-green-text bg-black rounded-2 p-1'>Inkomster</h3>
-            <p>Här har du möjlighet att registrera dina inkomster. När du har återkommande inkomster, så som lön eller barnbidrag så registrerar man det som sådant.</p>
-            <p>Övriga inkomster används för de inkomster som endast är tillfälliga, så som aktieutdelning.</p>
-          </Col>
+        <Row className='mb-5 align-items-center'>
           <Col className='text-center col-lg-6 col-12'>
-            <Image src='/homeimg2.png' alt='example image of incomes' className='img-fluid border border-2 border-light rounded-2 '/>
+            <Image src='/homeimg4.png' alt='example image of incomes' className='img-fluid border border-2 border-light rounded-2 '/>
           </Col>
+          <Col className='text-center col-lg-6 mt-3 col-12'>
+            <h3 className='mp-green-text bg-black rounded-2 p-1'>Budgettavla</h3>
+            <p>Budgettavlan är din centrala planeringsarena. 
+              Här skapar och anpassar du din ekonomiska plan genom att 
+              ställa in mål och budgetering för olika utgiftskategorier. 
+              Den ger dig en visuell överblick över din ekonomi, 
+              vilket gör det lättare att se hur väl din faktiska ekonomi 
+              överensstämmer med dina mål. 
+              Du kan enkelt justera och anpassa din budget löpande för 
+              att hålla dig på rätt spår mot dina spar- och utgiftsmål.</p>            
+          </Col>          
         </Row> 
 
-        <hr/>
+        <hr/>       
 
-        <Row>
-          <Col className='text-center col-lg-6 col-12'>
-            <Image src='/homeimg3.png' alt='example image of expenses' className='img-fluid border border-2 border-light rounded-2 '/>
-          </Col>
+        <Row className='mt-5 mb-5 align-items-center'>
           <Col className='text-center col-lg-6 mt-3 col-12'>
-            <h3 className='mp-green-text bg-black rounded-2 p-1'>Utgifter</h3>
-            <p>Alla dina utgifter har du möjlighet att registrera. Du kan kategorisera dina utgifter som är återkommande, så som hyra och andra avgifter.</p>
-            <p>Eller som övriga utgifter används för de utgifter som endast är tillfälliga, så som bilservice.</p>
-          </Col>
-        </Row>
-
-        <hr/>
-
-        <Row>
-          <Col className='text-center col-lg-6 mt-3 col-12'>
-            <h3 className='mp-green-text bg-black rounded-2 p-1'>Sammanfattning</h3>
-            <p>Efter registrering av inkomster och utgifter så har du möjlighet att generera en månadsanalys för att få en sammanställning av din ekonomi.</p>
-            <p>Du har inte möjlighet att generera en månadsanalys om du inte har några inkomster eller utgifter.</p>
+            <h3 className='mp-green-text bg-black rounded-2 p-1'>Månadsanalys</h3>
+            <p>Om du vill ha en analys för den månaden så behöver du registrera inkomster och utgifter.
+              I inkomstsektionen kan du registrera alla dina faktiska inkomstkällor. 
+              Det kan vara löneintäkter, bonusar, sidoprojekt eller andra inkomster. 
+              Genom att noggrant dokumentera dina inkomster får du en tydlig bild 
+              av din disponibla ekonomi och kan optimera din budget för att 
+              maximera sparandet eller investeringarna.</p>
+            <p>Genom att noggrant logga varje utgiftspost får du insikt i dina 
+              spenderingsvanor och kan identifiera områden där du kan spara eller 
+              optimera din budget. 
+              Det är en kraftfull metod för att säkerställa att dina utgifter 
+              är i linje med din planerade budget.</p>
+            <p>I sammanfattningen kan du generera en månadsanalys 
+              baserad på de inmatade uppgifterna från inkomster och utgifter. 
+              Här får du en översiktlig rapport som visar hur väl du 
+              har lyckats hålla dig till din planerade budget under en viss period. 
+              Det ger värdefulla insikter och hjälper dig att fatta informerade 
+              beslut för framtida ekonomisk planering.</p>
           </Col>
           <Col className='text-center col-lg-6 col-12'>
-            <Image src='/homeimg1.png' alt='example image of summary' className='img-fluid border border-2 border-light rounded-2 '/>
+            <Image src='/homeimg1.png' alt='example image of summary' className='img-fluid border border-2 border-light rounded-2'/>
           </Col>
         </Row>     
         </div>  
